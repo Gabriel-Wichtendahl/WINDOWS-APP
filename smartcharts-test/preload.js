602995ccfa56d1c8b57794081c008ff33f171852
Gabriel-Wichtendahl <118149051+Gabriel-Wichtendahl@users.noreply.github.com>
2026-06-16 19:11:12 -0300

    Delete smartcharts-test directory

--- smartcharts-test/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-contextBridge.exposeInMainWorld('desktopAPI', {
-  setPinned: (value) => ipcRenderer.invoke('set-pinned', value),
-  getPinned: () => ipcRenderer.invoke('get-pinned')
-});
